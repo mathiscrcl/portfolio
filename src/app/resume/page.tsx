d07087641e1d5c295e621bd9988b542a3bf70c09
Mathis Courcoul <comptecourcoul44170@gmail.com>
2025-09-03 12:50:36 +0200

    fix : resume page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,21 +1,15 @@
 import { Container } from "@/components/Container";
-import { Heading } from "@/components/Heading";
-import { Highlight } from "@/components/Highlight";
-import { Paragraph } from "@/components/Paragraph";
-import { Products } from "@/components/Products";
-import { WorkHistory } from "@/components/WorkHistory";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <Container>
-      <span className="text-4xl">💼</span>
-      <Heading className="font-black">Expériences professionnelles</Heading>
-      <Paragraph className="max-w-xl mt-4">
-        Je suis un développeur avec des expériences dans différents domaines, mais c&apos;est le 
-        <Highlight>data engineering</Highlight> qui me passionne vraiment et vers lequel je me dirige aujourd&apos;hui pour construire des solutions robustes de traitement de données
-      </Paragraph>
-      <WorkHistory />
+      <div className="w-full h-[800px]">
+        <iframe
+          src="/CV_Mathis_Courcoul.pdf"
+          title="CV"
+          className="w-full h-full border-0 rounded-md"
+        />
+      </div>
     </Container>
   );
 }
